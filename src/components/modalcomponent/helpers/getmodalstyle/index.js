8a233730aa4f7dbe8a75500cc9a4b1c64376da1c
carlosblancobolanos <cbolanos@ecotrak.com>
2020-10-15 17:43:00 -0600

    Saving code changes

--- src/components/modalcomponent/helpers/getmodalstyle/index.js
+++ src/components/modalcomponent/helpers/getmodalstyle/index.js
@@ -1,10 +1,6 @@
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-}
-
 export const getModalStyle = () => {
-    const top = 50 + rand();
-    const left = 50 + rand();
+    const top = 50
+    const left = 50
 
     return {
         top: `${top}%`,
