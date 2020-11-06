02f873c347a3ecb48c4043da22738d4bd573d86a
happyvirgo <happy0910virgo@outlook.com>
2020-11-06 09:07:52 -0600

    Saving code changes

---
+++ src/components/filteringcomponent/index.js
@@ -0,0 +1,30 @@
+/**
+ * Description: Create Filter Component
+ * Author: Carlos Blanco
+ * Created: 11/06/2020
+ * Ticket: ET-246
+ */
+
+//Basic Imports
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+//Material UI
+import Grid from '@material-ui/core/Grid';
+
+//Layouts
+import {
+
+} from './layouts'
+
+
+const FilteringComponent = ({ctadata}) => {
+
+    return (
+            <Grid container className={''}>
+
+            </Grid>
+    );
+};
+
+export default React.memo(FilteringComponent);
