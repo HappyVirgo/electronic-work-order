11256ce6dd511e7aa8cb76628be7a6b097032165
happyvirgo <happy0910virgo@outlook.com>
2020-12-09 12:49:12 -0600

    Save

--- src/components/datatablecomponent/layouts/reactwindowtablelayout/index.js
+++ src/components/datatablecomponent/layouts/reactwindowtablelayout/index.js
@@ -83,6 +83,7 @@ let savedData;
 export const ReactWindowTable = ({ data, columns, firstLoading }) => {
     const classes = useTableStyles();
     const itemData = createItemData(classes, columns, data, span);
+    console.log(data)
     if(!Object.is(data, savedData))
         scrollToTop(topOfList)
     savedData = data
