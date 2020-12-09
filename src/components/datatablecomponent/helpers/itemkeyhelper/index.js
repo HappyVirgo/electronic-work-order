5a4cf86f940d199698a51701bddf61c51ae5e0bd
happyvirgo <happy0910virgo@outlook.com>
2020-12-09 16:35:12 -0600

    Save

--- src/components/datatablecomponent/helpers/itemkeyhelper/index.js
+++ src/components/datatablecomponent/helpers/itemkeyhelper/index.js
@@ -1,5 +1,5 @@
 //Setting index as key to avoid weird behaviours on array mapping
 export const itemKey = (index, data) => {
-    console.log(data)
-    return data.items[index].workOrderId;
+    //console.log(data.items[index].workOrderId)
+    return data.items[index]?data.items[index].workOrderId:" ";
 }
